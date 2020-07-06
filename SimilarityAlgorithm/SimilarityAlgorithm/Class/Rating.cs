803b15bb7e03c009c6f83f58f5ac48b391d52eb2
VU Tuan Trung <trung.lespace@gmail.com>
2020-07-06 15:43:07 +0200

    Modify Rating property

--- SimilarityAlgorithm/SimilarityAlgorithm/Class/Rating.cs
+++ SimilarityAlgorithm/SimilarityAlgorithm/Class/Rating.cs
@@ -4,14 +4,12 @@ using System.Text;
 
 namespace SimilarityAlgorithm.Class
 {
-    public class Data
+    public class Rating
     {
         public int UserID { get; set; }
 
         public int MovieID { get; set; }
 
-        public int Rating { get; set; }
-
-        public DateTime TimeStamp { get; set; }
+        public int RatingNote { get; set; }
     }
 }
