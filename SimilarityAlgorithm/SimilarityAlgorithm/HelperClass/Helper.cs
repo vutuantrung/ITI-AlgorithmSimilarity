cc440e0499329f6439224c7ae51a331a03016964
VU Tuan Trung <trung.lespace@gmail.com>
2020-07-06 14:20:08 +0200

    Move methods to Helper class

---
+++ SimilarityAlgorithm/SimilarityAlgorithm/HelperClass/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimilarityAlgorithm.HelperClass
+{
+    public static class Helper
+    {
+        public static float[,] CalculateSimilarityMatrix( float[,] data )
+        {
+            var rows = data.GetLength(0);
+            var cols = data.GetLength(1);
+
+            float[,] similarityMatrix = new float[rows, cols];
+
+            for ( int i = 0; i < rows; i++ )
+            {
+                for ( int j = 0; j < cols; j++ )
+                {
+                    similarityMatrix[i, j] = Helper.GetCosineSimilarity( data, i, j );
+                }
+            }
+
+            return similarityMatrix;
+        }
+
+        public static float GetCosineSimilarity( float[,] matrix, int rowIdx, int colIdx )
+        {
+            int N = matrix.GetLength( 1 );
+
+            float dot = 0.0f;
+            float mag1 = 0.0f;
+            float mag2 = 0.0f;
+            for ( int n = 0; n < N; n++ )
+            {
+                dot += matrix[rowIdx, n] * matrix[colIdx, n];
+                mag1 += matrix[rowIdx, n] * matrix[rowIdx, n];
+                mag2 += matrix[colIdx, n] * matrix[colIdx, n];
+            }
+            return dot / (float)( Math.Sqrt( mag1 ) * Math.Sqrt( mag2 ) );
+        }
+    }
+}
--- SimilarityAlgorithm/SimilarityAlgorithm/Program.cs
+++ SimilarityAlgorithm/SimilarityAlgorithm/Program.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using SimilarityAlgorithm.HelperClass;
+using System;
 
 namespace SimilarityAlgorithm
 {
     class Program
     {
         #region Static variables
-        static float[,] maTrixUserBased = new float[,]
+        static float[,] maTrixUserBasedExample = new float[,]
         {
             { 4, 3, 0, 0, 5, 0 },
             { 5, 0, 4, 0, 4, 0 },
@@ -19,33 +20,9 @@ namespace SimilarityAlgorithm
 
         static void Main( string[] args )
         {
-            float[,] similarityMatrix = new float[6, 6];
-
-            for ( int i = 0; i < maTrixUserBased.GetLength( 1 ); i++ )
-            {
-                for ( int j = 0; j < maTrixUserBased.GetLength( 1 ); j++ )
-                {
-                    similarityMatrix[i, j] = GetCosineSimilarity( maTrixUserBased, i, j );
-                }
-            }
+            var similarityMatrix = Helper.CalculateSimilarityMatrix( maTrixUserBasedExample );
 
             Console.Read();
-        }
-
-        public static float GetCosineSimilarity( float[,] UserMatrix, int userAidx, int userBidx )
-        {
-            int N = UserMatrix.GetLength( 1 );
-
-            float dot = 0.0f;
-            float mag1 = 0.0f;
-            float mag2 = 0.0f;
-            for ( int n = 0; n < N; n++ )
-            {
-                dot += UserMatrix[userAidx, n] * UserMatrix[userBidx, n];
-                mag1 += UserMatrix[userAidx, n] * UserMatrix[userAidx, n];
-                mag2 += UserMatrix[userBidx, n] * UserMatrix[userBidx, n];
-            }
-            return dot / (float)( Math.Sqrt( mag1 ) * Math.Sqrt( mag2 ) );
         }
     }
 }
