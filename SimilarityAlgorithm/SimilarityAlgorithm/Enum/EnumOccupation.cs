69e4a577e4aef4d78126a519aeeb2acc34b8208f
VU Tuan Trung <trung.lespace@gmail.com>
2020-07-06 15:43:39 +0200

    Update Enum classes

---
+++ SimilarityAlgorithm/SimilarityAlgorithm/Enum/EnumOccupation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimilarityAlgorithm.Enum
+{
+    public enum Occupation
+    {
+        Administrator,
+        Artist,
+        Doctor,
+        Educator,
+        Engineer,
+        Entertainment,
+        Executive,
+        Healthcare,
+        Homemaker,
+        Lawyer,
+        Librarian,
+        Marketing,
+        None,
+        Other,
+        Programmer,
+        Retired,
+        Salesman,
+        Scientist,
+        Student,
+        Technician,
+        Writer,
+    }
+}
