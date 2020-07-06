d00c5b78daba404b41c4ebfc084a7eb465e3aa4a
VU Tuan Trung <trung.lespace@gmail.com>
2020-07-06 13:09:02 +0200

    Initialize project

---
+++ SimilarityAlgorithm/SimilarityAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimilarityAlgorithm
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "Hello World!" );
+        }
+    }
+}
