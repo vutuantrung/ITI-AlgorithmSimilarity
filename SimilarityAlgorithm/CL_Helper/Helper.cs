92efeb90e635e6e051bf19b6b1e9a6b909bb2bb9
VU Tuan Trung <trung.lespace@gmail.com>
2020-07-07 15:30:10 +0200

    Create Helper class library

---
+++ SimilarityAlgorithm/CL_Helper/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CL_Helper
+{
+    public static class Helper
+    {
+        public static string[] SplitString( this string textString, string splitString, StringSplitOptions stringSplitOptions = StringSplitOptions.None )
+        {
+            string[] stringArr = textString.Split( new string[] { splitString }, stringSplitOptions );
+            return stringArr;
+        }
+    }
+}
---
+++ SimilarityAlgorithm/CL_Helper/StaticData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CL_Helper
+{
+    public static class StaticData
+    {
+        public static string MoviesDataPath = @"C:\Users\VUTuanTrung\Documents\AlgorithmSimilarityDB\Moodle\TP1\movies.dat";
+        public static string PersonsDataPath = @"C:\Users\VUTuanTrung\Documents\AlgorithmSimilarityDB\Moodle\TP1\users.dat";
+        public static string RatingsDataPath = @"C:\Users\VUTuanTrung\Documents\AlgorithmSimilarityDB\Moodle\TP1\ratings.dat";
+        
+        public static string AirportDataPath = @"C:\Users\VUTuanTrung\Documents\AlgorithmSimilarityDB\Moodle\TP2\airports.txt";
+        public static string SchedulesDataPath = @"C:\Users\VUTuanTrung\Documents\AlgorithmSimilarityDB\Moodle\TP2\small-schedule.txt";
+    }
+}
